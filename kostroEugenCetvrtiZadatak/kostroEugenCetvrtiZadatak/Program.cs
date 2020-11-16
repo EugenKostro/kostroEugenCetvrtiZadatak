d8efee4c9d50566d17b82e20fa5be3a0fdc0148a
Eugen101 <55134543+EugenKostro@users.noreply.github.com>
2020-11-16 21:21:19 +0100

    Add files via upload

---
+++ kostroEugenCetvrtiZadatak/kostroEugenCetvrtiZadatak/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kostroEugenCetvrtiZadatak
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Upisite cijenu sa PDV-om: ");
+            double bezpdv = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Cijena sa PDV-om: "+(bezpdv+bezpdv*0,25));
+
+            Console.WriteLine("Upisite cijenu sa PDV-om: ");
+            double pdv = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Cijena bez PDV-a" + pdv * 0, 8);
+            Console.ReadKey();
+        }
+    }
+}
